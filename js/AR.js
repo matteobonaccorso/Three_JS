5c962f8cdb79013d9339e17a7ac1527641dc5322
matteobonaccorso <matteobonaccorso23@gmail.com>
2022-04-22 17:23:03 +0200

    try to detect images in AR

---
+++ js/AR.js
@@ -0,0 +1,92 @@
+//function to manage AR functionality
+async function activateXR() {
+    // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    const gl = canvas.getContext("webgl", { xrCompatible: true });
+
+    const scene = new THREE.Scene();
+
+    const light = new THREE.AmbientLight(0xeeeeee);
+    scene.add(light);
+
+    // Set up the WebGLRenderer, which handles rendering to the session's base layer.
+    const renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      preserveDrawingBuffer: true,
+      canvas: canvas,
+      context: gl
+    });
+    renderer.autoClear = false;
+
+    // The API directly updates the camera matrices.
+    // Disable matrix auto updates so three.js doesn't attempt
+    // to handle the matrices independently.
+    const camera = new THREE.PerspectiveCamera();
+    camera.matrixAutoUpdate = false;
+
+    // Initialize a WebXR session using "immersive-ar".
+    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'] });
+    session.updateRenderState({
+      baseLayer: new XRWebGLLayer(session, gl)
+    });
+
+    // A 'local' reference space has a native origin that is located
+    // near the viewer's position at the time the session was created.
+    const referenceSpace = await session.requestReferenceSpace('local');
+
+    // Create another XRReferenceSpace that has the viewer as the origin.
+    const viewerSpace = await session.requestReferenceSpace('viewer');
+    // Perform hit testing using the viewer as origin.
+    // const hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+
+    /*session.addEventListener("select", (event) => {
+      if (flower) {
+        const clone = flower.clone();
+        clone.position.copy(reticle.position);
+        scene.add(clone);
+      }
+    })*/
+
+    // Create a render loop that allows us to draw on the AR view.
+    const onXRFrame = (time, frame) => {
+      // Queue up the next draw request.
+      session.requestAnimationFrame(onXRFrame);
+
+      const group = new THREE.InteractiveGroup(renderer, camera);
+      scene.add(group);
+
+      const squares = detectImages(canvas);
+
+      for(uiContent of squares){
+        let object = new THREE.HTMLMesh(uiContent);
+        object.scale.setScalar(2);
+
+        group.add(object);
+      }
+
+      // Bind the graphics framebuffer to the baseLayer's framebuffer
+      gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
+
+      // Retrieve the pose of the device.
+      // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
+      const pose = frame.getViewerPose(referenceSpace);
+      if (pose) {
+        // In mobile AR, we only have one view.
+        const view = pose.views[0];
+
+        const viewport = session.renderState.baseLayer.getViewport(view);
+        renderer.setSize(viewport.width, viewport.height)
+
+        // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+        camera.matrix.fromArray(view.transform.matrix)
+        camera.projectionMatrix.fromArray(view.projectionMatrix);
+        camera.updateMatrixWorld(true);
+
+        // Render the scene with THREE.WebGLRenderer.
+        renderer.render(scene, camera)
+      }
+    }
+
+    session.requestAnimationFrame(onXRFrame);
+}
--- js/imageDetection.js
+++ js/imageDetection.js
@@ -1,7 +1,4 @@
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
-const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+//functions to manage image detection
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
@@ -12,22 +9,17 @@ function getUserMediaSupported() {
 // If webcam supported, add event listener to button for when user
 // wants to activate it to call enableCam function which we will 
 // define in the next step.
-if (getUserMediaSupported()) {
+/*if (getUserMediaSupported()) {
     enableWebcamButton.addEventListener('click', enableCam);
 } else {
     console.warn('getUserMedia() is not supported by your browser');
-}
+}*/
 
-var children = [];
-
-function predictWebcam() {
+function detectImages(source) {
   // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
+  const squares = model.detect(source).then(function (predictions) {
+    // Store the highlighting square
+    let children = [];
     
     // Now lets loop through predictions and draw them to the live view if
     // they have a high confidence score.
@@ -49,20 +41,22 @@ function predictWebcam() {
             + predictions[n].bbox[2] + 'px; height: '
             + predictions[n].bbox[3] + 'px;';
 
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
+        // document.body.appendChild(highlighter);
+        // document.body.appendChild(p);
         children.push(highlighter);
         children.push(p);
-      }
+      } 
     }
+
+    return children;
     
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
   });
+
+  return squares;
 }
   
 // Placeholder function for next step. Paste over this in the next step.
-function enableCam(event) {
+/*function enableCam(event) {
      // Only continue if the COCO-SSD has finished loading.
   if (!model) {
     return;
@@ -81,18 +75,4 @@ function enableCam(event) {
     video.srcObject = stream;
     video.addEventListener('loadeddata', predictWebcam);
   });
-}
-
-// Store the resulting model in the global scope of our app.
-var model = undefined;
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
+}*/
